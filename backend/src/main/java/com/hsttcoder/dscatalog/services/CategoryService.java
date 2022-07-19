129cb67ddbef2f0fbe919c28f082b2df1773b00d
hstt-coder <eduardolopes19@outlook.com>
2022-07-19 20:20:19 -0300

    Pagination

--- backend/src/main/java/com/hsttcoder/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/hsttcoder/dscatalog/services/CategoryService.java
@@ -1,14 +1,15 @@
 package com.hsttcoder.dscatalog.services;
 
-import java.util.List;
+
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,10 +26,11 @@ public class CategoryService {
 	private CategoryRepository repository;
 
 	@Transactional(readOnly = true)
-	public List<CategoryDTO> findAll() {
+	public Page<CategoryDTO> findAllPaged(PageRequest pageRequest) {
 
-		List<Category> list = repository.findAll();
-		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+		Page<Category> list = repository.findAll(pageRequest);
+		
+		return list.map(x -> new CategoryDTO(x));
 
 	}
 
