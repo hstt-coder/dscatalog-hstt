d524ce7f03f45aa799d46af9dd48855a571e35ba
hstt-coder <eduardolopes19@outlook.com>
2022-08-08 16:36:22 -0300

    Custom validation Response

--- backend/src/main/java/com/hsttcoder/dscatalog/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/hsttcoder/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -6,10 +6,11 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import com.hsttcoder.dscatalog.services.exceptions.DbException;
 import com.hsttcoder.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
@@ -30,16 +31,20 @@ public class ResourceExceptionHandler {
 		return ResponseEntity.status(status).body(err);
 	}
 	
-	@ExceptionHandler(DbException.class)
-	public ResponseEntity<StandardError> database(DbException e, HttpServletRequest request) {
-		StandardError err = new StandardError();
-		HttpStatus status = HttpStatus.BAD_REQUEST;
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ValidationError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
+		ValidationError err = new ValidationError();
+		HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
 		
 		err.setTimestamp(Instant.now());
 		err.setStatus(status.value());
-		err.setError("Database Exception");
+		err.setError("Validation Exception");
 		err.setMessage(e.getMessage());
 		err.setPath(request.getRequestURI());
+		
+		for (FieldError f : e.getBindingResult().getFieldErrors()) {
+			err.addError(f.getField(), f.getDefaultMessage());
+		}
 		
 		return ResponseEntity.status(status).body(err);
 	}
